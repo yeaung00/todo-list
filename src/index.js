791f8444a73e4bdd5f0fd9a4b8e4b75607274bd6
yeaung00 <yep.aung00@gmail.com>
2022-08-15 21:35:39 -0700

    Created setCurrentTitle function.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 const Nav = (() => {
+    let currentTitle;
     //cache DOM
     const projectDiv = document.getElementById("projectDiv");
     const addProjectButton = document.getElementById("addProjectButton");
@@ -12,9 +13,18 @@ const Nav = (() => {
     addProjectButton.addEventListener("click", toggleHidden);
     cancelButton.addEventListener("click", toggleHidden);
     submitButton.addEventListener("click", toggleHidden);
+    submitButton.addEventListener("click", setCurrentTitle);
+    submitButton.addEventListener("click", createProject);
 
     function toggleHidden() {
         addProjectButton.classList.toggle("hidden");
         projectDescriptionDiv.classList.toggle("hidden");
+    }
+    function setCurrentTitle(e) {
+        if (e.target.id === "submitButton") { currentTitle = projectDescriptionInput.value; }
+        else { currentTitle = e.target.textContent; }
+    }
+    function createProject() {
+
     }
 })();
