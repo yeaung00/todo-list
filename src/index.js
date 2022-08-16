c38be6944fbfa01f8570c7a17b5da572b4f899a4
yeaung00 <yep.aung00@gmail.com>
2022-08-15 21:28:41 -0700

    Cached DOM for Nav

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const Nav = (() => {
+    //cache DOM
+    const projectDiv = document.getElementById("projectDiv");
+    const addProjectButton = document.getElementById("addProjectButton");
+    const projectDescriptionDiv = document.getElementById("projectDescriptionDiv");
+    const cancelButton = document.getElementById("cancelButton");
+    const submitButton = document.getElementById("submitButton");
+    const projectDescriptionInput = document.getElementById("projectDescriptionInput");
+})();
