7d40998a895521ce849e3a1f0ef06210099f7ab0
yeaung00 <yep.aung00@gmail.com>
2022-08-15 21:31:55 -0700

    Created toggleHidden event for cancel and submit button.

--- src/index.js
+++ src/index.js
@@ -8,4 +8,13 @@ const Nav = (() => {
     const cancelButton = document.getElementById("cancelButton");
     const submitButton = document.getElementById("submitButton");
     const projectDescriptionInput = document.getElementById("projectDescriptionInput");
+    //bind events
+    addProjectButton.addEventListener("click", toggleHidden);
+    cancelButton.addEventListener("click", toggleHidden);
+    submitButton.addEventListener("click", toggleHidden);
+
+    function toggleHidden() {
+        addProjectButton.classList.toggle("hidden");
+        projectDescriptionDiv.classList.toggle("hidden");
+    }
 })();
